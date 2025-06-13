7827e11cba61618a4664b510095765bd4aab1447
DjRed <67414617+GVERES74@users.noreply.github.com>
2025-06-13 13:47:13 +0200

    Update UserData.java

--- src/main/java/Model/Tables/UserData.java
+++ src/main/java/Model/Tables/UserData.java
@@ -13,61 +13,34 @@ import javafx.beans.property.SimpleStringProperty;
 public class UserData {
   
     
-    private final SimpleStringProperty userid, usercatid, username, userphone, useremail, usercompanyid, userpassword;
+    private final SimpleStringProperty userid, username, userpassword personid;
     
     
-    public UserData(Integer userId, String userCatId, String userName, String userPhone, String userEmail, String userCompanyId, String userPassword){
+    public UserData(Integer userId, String userName, String userPassword, Integer personId){
         
         this.userid = new SimpleStringProperty(String.valueOf(userId));
-        this.usercatid = new SimpleStringProperty(userCatId);
         this.username = new SimpleStringProperty(userName);
-        this.userphone = new SimpleStringProperty(userPhone);
-        this.useremail = new SimpleStringProperty(userEmail);
-        this.usercompanyid = new SimpleStringProperty(userCompanyId);
         this.userpassword = new SimpleStringProperty(userPassword);
+        this.personid = new SimpleStringProperty(personId);
         
     }
 
     
-     public UserData(String userCatId, String userName, String userPhone, String userEmail, String userCompanyId, String userPassword){
-        
-        this.userid = new SimpleStringProperty("");
-        this.usercatid = new SimpleStringProperty(userCatId);
-        this.username = new SimpleStringProperty(userName);
-        this.userphone = new SimpleStringProperty(userPhone);
-        this.useremail = new SimpleStringProperty(userEmail);
-        this.usercompanyid = new SimpleStringProperty(userCompanyId);
-        this.userpassword = new SimpleStringProperty(userPassword);
-    
-    }
-    
+         
     public String getUserid() {
         return userid.get();
-    }
-
-    public String getUsercatid() {
-        return usercatid.get();
     }
 
     public String getUsername() {
         return username.get();
     }
-
-    public String getUserphone() {
-        return userphone.get();
-    }
-
-    public String getUseremail() {
-        return useremail.get();
-    }
-
-    public String getUsercompanyid() {
-        return usercompanyid.get();
-    }
-
+    
     public String getUserpassword() {
         return userpassword.get();
     }
 
-    
+    public String getPersonid() {
+        return personid.get();
+    }
+  
 }
