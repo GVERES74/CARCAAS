c7b2e39bb247db40f89b5afa707e9e9b1dfcb901
DjRed <67414617+GVERES74@users.noreply.github.com>
2025-06-26 14:26:28 +0200

    Update VeterinaryData.java
    fields for taxnumber and bank account number added

--- src/main/java/Model/Tables/VeterinaryData.java
+++ src/main/java/Model/Tables/VeterinaryData.java
@@ -15,19 +15,22 @@ public class VeterinaryData {
    
     private SimpleStringProperty veterinaryid;
     private SimpleStringProperty veterinaryname;
+    private SimpleStringProperty veterinarytaxnumber;
+    private SimpleStringProperty veterinarybankaccountnumber;
     private SimpleStringProperty personaddressid;
         
         
-    public VeterinaryData(Integer veterinaryId, String veterinaryName, Integer personAddressId){
+    public VeterinaryData(Integer veterinaryId, String veterinaryName, String veterinaryTaxnumber, String veterinaryBankaccountnumber, Integer personAddressId){
         
         this.veterinaryid = new SimpleStringProperty(String.valueOf(veterinaryId));
-        this.veterinaryname = new SimpleStringProperty(veterinaryName);        
+        this.veterinarytaxnumber = new SimpleStringProperty(veterinaryTaxnumber);
+        this.veterinarybankaccountnumber = new SimpleStringProperty(veterinaryBankaccountnumber);
         this.personaddressid = new SimpleStringProperty(String.valueOf(personAddressId));
         
     }
 
        
-   //insert code getters
+   
 
     public String getVeterinaryid() {
         return veterinaryid.get();
@@ -37,6 +40,10 @@ public class VeterinaryData {
         return veterinaryname.get();
     }
 
+//insert code getters for taxnumber and bankaccount number
+
+
+   
     public String getPersonaddressid() {
         return personaddressid.get();
     }
