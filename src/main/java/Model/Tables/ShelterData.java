a46a7daacc322d129ea681b441f06c544af4f978
DjRed <67414617+GVERES74@users.noreply.github.com>
2025-06-26 14:23:23 +0200

    Update ShelterData.java
    fields for Taxnumber and Bank account number added

--- src/main/java/Model/Tables/ShelterData.java
+++ src/main/java/Model/Tables/ShelterData.java
@@ -15,22 +15,24 @@ public class ShelterData {
    
     private SimpleStringProperty shelterid;
     private SimpleStringProperty sheltername;
+    private SimpleStringProperty sheltertaxnumber;
+    private SimpleStringProperty shelterbankaccountnumber;
     private SimpleStringProperty animalid;
     private SimpleStringProperty personaddressid;
         
         
-    public ShelterData(Integer shelterId, String shelterName, Integer animalId, Integer personAddressId){
+    public ShelterData(Integer shelterId, String shelterName, String shelterTaxnumber, String shelterBankaccountnumber, Integer animalId, Integer personAddressId){
         
         this.shelterid = new SimpleStringProperty(String.valueOf(shelterId));
         this.sheltername = new SimpleStringProperty(shelterName);        
+        this.sheltername = new SimpleStringProperty(shelterTaxnumber);        
+        this.sheltername = new SimpleStringProperty(shelterBankaccountnumber);        
         this.animalid = new SimpleStringProperty(String.valueOf(animalId));
         this.personaddressid = new SimpleStringProperty(String.valueOf(personAddressId));
         
     }
 
-       
-   //insert code getters
-
+   
     public String getShelterid() {
         return shelterid.get();
     }
@@ -39,6 +41,11 @@ public class ShelterData {
         return sheltername.get();
     }
 
+
+//insert code getters for taxnumber and bankaccountnumber
+
+
+   
     public String getAnimalid() {
         return animalid.get();
     }
